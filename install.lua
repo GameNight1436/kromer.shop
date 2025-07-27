19bfaa61bbe68f03618932332d6bdf919d72e55e
Greenhoh1 <derekjlucier@gmail.com>
2025-07-27 15:30:22 -0400

    update install.lua to download files from the dev branch instead of main

--- install.lua
+++ install.lua
@@ -1,12 +1,12 @@
 print("Downloading KTWSL..")
-shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/main/ktwsl.lua ktwsl.lua")
+shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/dev/ktwsl.lua ktwsl.lua")
 
 print("Downloading uninstall.lua")
-shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/main/uninstall.lua uninstall.lua")
+shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/dev/uninstall.lua uninstall.lua")
 
 print("Downloading kromer.shop")
-shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/main/kromer.shop.lua kromer.shop.lua")
+shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/dev/kromer.shop.lua kromer.shop.lua")
 
 print("Downloading example configs")
-shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/main/config config")
-shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/main/listings listings")
+shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/dev/config config")
+shell.run("wget https://raw.githubusercontent.com/GameNight1436/kromer.shop/refs/heads/dev/listings listings")
